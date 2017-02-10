f31a5519bbc14572a152b449bec279b2c46fa1a1
wdye <wdye@pindrop.com>
2017-02-10 18:04:30 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject scrabble "0.1.0-SNAPSHOT"
+  :description "Scrabble in Clojure"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot scrabble.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/scrabble/core_test.clj
@@ -0,0 +1,7 @@
+(ns scrabble.core-test
+  (:require [clojure.test :refer :all]
+            [scrabble.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
