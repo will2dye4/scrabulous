2fa2118cb7f83c50073a5c2ad55f889a18826c6c
wdye <wdye@pindrop.com>
2017-02-10 18:13:14 -0500

    Renamed scrabble to scrabulous

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject scrabble "0.1.0-SNAPSHOT"
+(defproject scrabulous "0.1.0-SNAPSHOT"
   :description "Scrabble in Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot scrabble.core
+  :main ^:skip-aot scrabulous.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/scrabble/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns scrabble.core-test
-  (:require [clojure.test :refer :all]
-            [scrabble.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
