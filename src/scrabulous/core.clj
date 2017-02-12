ec29eac1a3933aae92c0c5a499852bc9420ff32c
William Dye <will2dye4@mac.com>
2017-02-11 19:59:29 -0500

    Code cleanup and generification

--- src/scrabulous/core.clj
+++ src/scrabulous/core.clj
@@ -26,9 +26,25 @@
    9 #{\A \I}
    12 #{\E}})
 
-(def board (atom (vec (repeat (* dim dim) nil))))
+(def valid-words
+  (with-open [reader (io/reader (io/resource "words.txt"))]
+    (->> (line-seq reader)
+      (keep #(re-matches #"[a-zA-Z]+" %))
+      set)))
 
-(def columns (vec (map (comp str char) (range 65 80))))   ;; ASCII values for 'A' - 'O'
+(defn create-board
+  ([] (create-board dim))
+  ([dim] (vec (repeat (* dim dim) nil))))
+
+(def board (atom (create-board)))
+
+(defn get-dim [board] (int (Math/sqrt (count board))))
+
+(defn get-columns
+  ([] (get-columns dim))
+  ([dim] (vec (map (comp str char) (range (int \A) (+ (int \A) dim))))))
+
+(def columns (get-columns))
 
 (defn column-index [column] (.indexOf columns (string/upper-case column)))
 
@@ -36,8 +52,46 @@
 
 (defn next-column [column] (column-name (inc (column-index column))))
 
-(defn get-offset [[column row]]
-  (+ (* dim (dec row)) (column-index column)))
+(defn get-index
+  ([coordinates] (get-index coordinates dim))
+  ([[column row] dim]
+    (let [column (if (number? column) (dec column) (column-index column))]
+      (+ (* dim (dec row)) column))))
+
+(defn create-tile-bag
+  ([] (create-tile-bag letter-frequencies))
+  ([letter-frequencies]
+    (-> (for [[number letters] letter-frequencies letter letters] (repeat number letter))
+      flatten
+      shuffle
+      vec)))
+
+(def tile-bag (atom (create-tile-bag)))
+
+(defn draw-tiles
+  ([n] (draw-tiles tile-bag n))
+  ([bag n]
+    (let [tiles (take n @bag)]
+      (swap! bag #(vec (drop n %)))
+      (vec tiles))))
+
+(defn tile-rack
+  ([] (tile-rack tile-bag))
+  ([bag] (draw-tiles bag tiles-per-player)))
+
+(defn place
+  ([coordinates letter] (place board coordinates letter))
+  ([board coordinates letter]
+    (swap! board assoc (get-index coordinates (get-dim @board)) (string/upper-case letter))))
+
+(defn place-word
+  ([coordinates direction word] (place-word board coordinates direction word)) 
+  ([board [column row :as coordinates] direction word]
+    (if (empty? word)
+      @board
+      (let [letter (first word) column (if (number? column) column (inc (column-index column)))]
+        (place board coordinates letter)
+        (recur board (if (= direction :across) [(inc column) row] [column (inc row)]) direction (rest word))))))
 
 (defn letter-score [letter]
   (let [letter (if (char? letter) letter (.charAt letter 0))]
@@ -45,49 +99,26 @@
 
 (defn word-score [word] (reduce + (map letter-score word)))
 
-(def tile-bag
-  (-> (for [[number letters] letter-frequencies letter letters] (repeat number letter))
-    vec
-    flatten
-    shuffle
-    atom))
+(defn print-board
+  ([] (print-board board))
+  ([board]
+    (let [dim (get-dim @board)
+          builder (StringBuilder.)
+          separator (str "    +" (apply str (repeat dim "---+")) \newline)]
+      (.append builder separator)
+      (doseq [[idx row] (map-indexed vector (partition dim @board))]
+        (.append builder (str (if (< idx 9) "  " " ") (inc idx) " |"))
+        (doseq [square row]
+          (.append builder (str " " (or square " ") " |")))
+        (.append builder "\n")
+        (.append builder separator))
+      (.append builder (apply str "      " (string/join "   " (get-columns dim))))
+      (println (.toString builder)))))
 
-(defn draw-tiles [bag n]
-  (let [tiles (take n @bag)]
-    (swap! bag #(vec (drop n %)))
-    (vec tiles)))
-
-(defn tile-rack [bag] (draw-tiles bag tiles-per-player))
-
-(def valid-words
-  (with-open [reader (io/reader (io/resource "words.txt"))]
-    (->> (line-seq reader)
-      (into [])
-      (keep #(re-matches #"[a-zA-Z]+" %))
-      set)))
-
-(defn place [board coordinates letter]
-  (swap! board assoc (get-offset coordinates) (string/upper-case letter)))
-
-(defn place-word [board [column row :as coordinates] direction word]
-  (if (empty? word)
-    nil
-    (let [letter (first word)]
-      (place board coordinates letter)
-      (recur board (if (= direction :across) [(next-column column) row] [column (inc row)]) direction (rest word)))))
-
-(defn print-board [board]
-  (let [builder (StringBuilder.)
-        separator "    +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+\n"]
-    (.append builder separator)
-    (doseq [[idx row] (map-indexed vector (partition dim @board))]
-      (.append builder (str (if (< idx 9) "  " " ") (inc idx) " |"))
-      (doseq [square row]
-        (.append builder (str " " (or square " ") " |")))
-      (.append builder "\n")
-      (.append builder separator))
-    (.append builder "      A   B   C   D   E   F   G   H   I   J   K   L   M   N   O")
-    (println (.toString builder))))
+(defn print-rack
+  ([rack] (print-rack rack true))
+  ([rack sort?]
+    (println (str "[" (string/join " " (if sort? (sort rack) rack)) "]"))))
 
 (defn -main
   "I don't do a whole lot ... yet."
