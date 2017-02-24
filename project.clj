ae84cecd7525761abcfbbd7a8575b313ccb7665e
wdye <wdye@pindrop.com>
2017-02-24 17:53:49 -0500

    Added JSON serializer

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.namespace "0.2.11"]]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [cheshire "5.7.0"]]
   :main ^:skip-aot scrabulous.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
---
+++ src/scrabulous/serializer.clj
@@ -0,0 +1,23 @@
+(ns scrabulous.serializer
+  (:require [scrabulous.board :refer [get-dim]]
+            [clojure.java.io :as io]
+            [cheshire.core :as json]))
+
+(defn game->json
+  "Converts game to the expected JSON format"
+  ([game]
+    (let [board (:board game)
+          dim (get-dim board)
+          new-board (->> board
+                      (map #(if (= "_" %) "*" (or % "")))
+                      (partition dim)
+                      (map vec)
+                      (into []))
+          moves (vec (map (fn [[_ player]] (vec (map :total (:moves player)))) (:players game)))]
+      {:board new-board
+       :moves moves})))
+
+(defn save-game
+  "Saves game to filepath"
+  ([game filepath]
+   (json/generate-stream (game->json game) (io/writer filepath))))
