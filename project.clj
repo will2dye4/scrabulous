5be158e67d53e695f2028b68a9a8936715a915b0
William Dye <will2dye4@mac.com>
2017-03-06 22:41:29 -0500

    Added logging stuff

--- project.clj
+++ project.clj
@@ -4,8 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
-                 [cheshire "5.7.0"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [cheshire "5.7.0"]
+                 ;; Java stuff
+                 [org.slf4j/slf4j-log4j12 "1.7.1"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]]
   :main ^:skip-aot scrabulous.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
